9fa66a4cf87ee088c4c07a2d92d4382a943f021d
Eli Da Costa <costa141@hotmail.co.uk>
2023-06-03 14:14:09 +0100

    Businesses controller routes complete-refactored

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :businesses
+  resources :businesses, only: %i[index show] # refactored resources code.
 end
