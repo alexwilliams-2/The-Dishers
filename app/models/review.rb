3d8a342dd68748ca0da199908cdec35d5f96bf64
keti kaleja <katykaleja@gmail.com>
2023-06-03 11:30:25 +0100

    Adding validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :business
   belongs_to :user
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: 1..5 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :user_favourites, dependent: :destroy
 
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6}
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
