007a8a95d16f258413c6b3879d6ec1ad0229f8b5
keti kaleja <katykaleja@gmail.com>
2023-06-03 11:37:16 +0100

    Adding validation to review

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,6 @@ class Review < ApplicationRecord
 
   validates :title, presence: true
   validates :content, presence: true
+  validates :job_title, presence: true
   validates :rating, presence: true, inclusion: { in: 1..5 }
 end
