05c5719966b1ecdbb2946afcb612841fc33e626a
keti kaleja <katykaleja@gmail.com>
2023-06-03 14:26:09 +0100

    Creating users controller and view. Fixing bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,6 @@ class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :user_favourites, dependent: :destroy
 
-  validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6}
-
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable :validatable
 end
