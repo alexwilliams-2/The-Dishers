d525f14fe1162c6009cac35724c91996f7941b9f
keti kaleja <katykaleja@gmail.com>
2023-06-03 16:43:27 +0100

    Adding cloudinary for user's avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+  has_one_attached :photo
+
   has_many :reviews, dependent: :destroy
   has_many :user_favourites, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable :validatable
+         :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users
   resources :businesses, only: %i[index show] # refactored resources code.
 end
