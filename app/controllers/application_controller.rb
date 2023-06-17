7ec2cbafddcb7e3f3572108c94cf24fef1780607
Alex Williams <ahnnwilliams@hotmail.co.uk>
2023-06-17 10:50:31 +0100

    margins sorted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:photo, :location, :bio, :username])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :location, :bio, :username])
   end
 end
