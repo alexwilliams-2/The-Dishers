24108a86f8d2ac2e56308ef9f00fc36600c362b1
maram allaghi <writtenbymaram@gmail.com>
2023-06-29 17:16:10 +0100

    styled profile

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     resources :reviews, only: [:create]
   end # refactored resources code.
 
+
   resources :reviews, only: [:destroy]
 
   resources :chats, only: [:show, :index] do
