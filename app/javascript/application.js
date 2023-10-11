6c4e85d956deb531245aeea5f1a6db17c9888c76
maram allaghi <writtenbymaram@gmail.com>
2023-10-11 22:08:07 +0300

    added import lines to application.scss

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,3 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import 'tailwindcss/base';
-import 'tailwindcss/components';
-import 'tailwindcss/utilities';
