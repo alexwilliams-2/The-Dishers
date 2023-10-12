a0418b0ab65aa052bc9a7ff0237f0542ead80e83
keti kaleja <katykaleja@gmail.com>
2023-10-12 21:02:04 +0100

    Adding wages filter and styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,3 @@
 module ApplicationHelper
   include Pagy::Frontend
-
-  def wage_checkbox(wage_range, label)
-    checked = params[:wage]&.include?(wage_range)
-    label_tag "wage_#{wage_range}" do
-      check_box_tag 'wage[]', wage_range, checked
-      label
-    end
-  end
 end
