796a546f3f614fcb04519deefc2352d1a75e9de5
Alex Williams <ahnnwilliams@hotmail.co.uk>
2023-10-12 21:27:13 +0100

    created validation method

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,19 @@
+require 'language_filter'
+
 class Review < ApplicationRecord
   belongs_to :business
   belongs_to :user
-
-  validates :title, presence: true
   validates :content, presence: true
   validates :job_title, presence: true
   validates :rating, presence: true, inclusion: { in: 1..5 }
+
+  validate :profanity
+
+  def profanity
+    profanity_filter = LanguageFilter::Filter.new matchlist: File.join(Rails.root, '/config/language_filter.yml')
+    if profanity_filter.match?(content)
+      errors.add(:content, 'Hi there - you can be critical but please keep your review informative and hate-free. Thanks!')
+    end
+  end
+
 end
