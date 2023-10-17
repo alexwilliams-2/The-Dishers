fa71c420d271e0b5c9b154db4e0703fa37181ef4
Alex Williams <ahnnwilliams@hotmail.co.uk>
2023-10-17 20:26:01 +0100

    added too short method in review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -13,18 +13,20 @@ class Review < ApplicationRecord
   def profanity
     profanity_filter = LanguageFilter::Filter.new matchlist: File.join(Rails.root, 'config/language_filter_profanity.yml')
     if profanity_filter.match?(content)
-      errors.add(:base, 'Mind your language! Whilst we encourage healthy criticism, please keep your review informative and hate-free for our community.')
+      errors.add(:base, 'Mind your language! Whilst we encourage healthy criticism, we do not condone profanity. Please keep your review informative and hate-free for our community.')
     end
   end
 
   def named_individual
-    profanity_filter = LanguageFilter::Filter.new matchlist: File.join(Rails.root, '/config/language_filter.yml')
+    profanity_filter = LanguageFilter::Filter.new matchlist: File.join(Rails.root, '/config/language_filter_names.yml')
     if profanity_filter.match?(content)
-      errors.add(:base, 'We unfortunately do not allow you to name colleagues or customers, even if that manager was always out for a smoke break. Please keep your review informative without naming names.')
+      errors.add(:base, "The Dishers values anonymonity for it's users. Colleagues and customers also deserve a right to privacy, even if that manager was always out for a smoke break. Please keep your review informative without naming names.")
     end
   end
 
-
-
-
+  def too_short
+    if content[120] == false
+      errors.add(:base, "Your review was too short to be submitted! The Dishers works when explain our experiences - please go into more detail for your fellow Dishers!")
+    end
+  end
 end
