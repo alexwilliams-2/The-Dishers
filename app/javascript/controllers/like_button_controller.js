2300eaacbb0402db49d72c0d43c081b12b39f1a1
maram allaghi <writtenbymaram@gmail.com>
2023-10-24 21:45:49 +0300

    fixing stimulus

--- app/javascript/controllers/like_button_controller.js
+++ app/javascript/controllers/like_button_controller.js
@@ -6,40 +6,13 @@ export default class extends Controller {
 
   connect() {
     console.log("hey");
-    this.updateSvgClass();
+
   }
-
-  // fillheart(e) {
-  //   this.likeButtonTarget.classList.toggle("heart-filled");
-
-  // }
 
   fillheart(e) {
-    const businessId = this.data.get("business_id"); // Assuming you have a data attribute for business ID
-    const url = `/businesses/${businessId}`; // Replace with your actual route
+    this.likeButtonTarget.classList.toggle("heart-filled");
 
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    .then(response => response.json())
-    .then(data => {
-      const isLiked = data.is_liked; // Replace with the actual key in your response
-      if (isLiked) {
-        this.likeButtonTarget.classList.add("heart-filled");
-      } else {
-        this.likeButtonTarget.classList.remove("heart-filled");
-      }
-    })
-    .catch(error => console.error('Error:', error));
   }
 
 
-  updateSvgClass() {
-    const isLiked = this.likeButtonTarget.classList.contains("heart-filled");
-    const svgClass = isLiked ? 'heart-filled' : 'heart';
-    this.likeButtonTarget.setAttribute('class', svgClass);
-  }
 }
